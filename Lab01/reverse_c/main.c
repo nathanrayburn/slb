05e098ce45dd35bb750fd313bbd229a801f9f82f
unknown <nathanarmand@hotmail.com>
2024-10-21 01:57:01 +0200

    update

--- Lab01/reverse_c/main.c
+++ Lab01/reverse_c/main.c
@@ -2,52 +2,68 @@
 #include <sys/types.h>
 
 void encrypt0(FILE* fp) {
-    uint readCharFromFile;
+    int readCharFromFile;
     char res;
-    while(true) {
+    while (true) {
         readCharFromFile = fgetc(fp);
-        if(readCharFromFile == 0xffffffff) break;
-        fseek(fp, -1, 1);
-        if((readCharFromFile & 2) == 0) {
-            res = (int)(char)readCharFromFile ^ 0xef;
-        }else {
-            res = (int)(char)readCharFromFile ^ 0xeb;
+        if (readCharFromFile == EOF) break;
+        fseek(fp, -1, SEEK_CUR);
+        if ((readCharFromFile & 2) == 0) {
+            res = (char)readCharFromFile ^ 0xef;
+        } else {
+            res = (char)readCharFromFile ^ 0xeb;
         }
-        fputc(res,fp);
+        fputc(res, fp);
     }
-
 }
 
 void decrypt0(FILE* fp) {
     uint readCharFromFile;
     char res;
-    while(true) {
+    if (fp == NULL) return;
+    rewind(fp);
+    while (true) {
         readCharFromFile = fgetc(fp);
-
-        if(readCharFromFile == 0xffffffff) break;
-
-        fseek(fp, -1, 1);
-
-        res = (int)(char)readCharFromFile ^ 0xef;
-
-        if((res & 2) == 0){
+        if (readCharFromFile == EOF) {
+            printf("End of file\n");
+            break;
+        }
+        fseek(fp, -1, SEEK_CUR);
+        res = (char)readCharFromFile ^ 0xef;
+        if ((res & 2) == 0) {
             fputc(res, fp);
             continue;
         }
-
-        res = (int)(char)readCharFromFile ^ 0xbe;
-        fputc(res,fp);
+        res = (char)readCharFromFile ^ 0xbe;
+        fputc(res, fp);
     }
 }
+
 int main(void) {
-
-    FILE* fp = fopen("salary.txt", "rb+");
-
+    FILE* fp = fopen("/home/nathan/Documents/git/slb/Lab01/reverse_c/salary.txt", "rb+");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
     decrypt0(fp);
+    fclose(fp);
 
-    fp = fopen("password.txt", "rb+");
+    /*
+    fp = fopen("passwords.txt", "rb");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
+    // Call encrypt0 or decrypt0 as needed
+    fclose(fp);
 
-    fp = fopen("confdiential.txt", "wb+");
-
+    fp = fopen("confidential.txt", "wb+");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
+    // Call encrypt0 or decrypt0 as needed
+    fclose(fp);
+        */
     return 0;
 }
